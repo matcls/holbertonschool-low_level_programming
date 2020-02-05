9a2f5d757273a4874c032cda592db9393c4da2a6
matcls <manueltorresvesga@gmail.com>
2020-02-05 17:40:50 -0500

    added files for mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 /**
- * main: Prints size of various types
- *
- * Return: Always 0 (Success)
- */
+* main: Prints size of various types
+*
+* Return: Always 0
+*/
 
 int main(void)
 {
@@ -13,5 +13,6 @@ printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
 return (0);
 }
