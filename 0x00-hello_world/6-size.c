1bbe54d391db9368c37b67d60b8bcf81c8e5b9af
matcls <manueltorresvesga@gmail.com>
2020-02-05 17:45:38 -0500

    added files for mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
- printf("\n");
+printf("\n");
 return (0);
 }
