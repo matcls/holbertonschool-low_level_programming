be14bb9aaf4a29d92822339a86ca06aa4ed8f358
matcls <manueltorresvesga@gmail.com>
2020-02-05 17:57:25 -0500

    added files for mandatory tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
 /**
 * main: print the content of the line.
 *
+*Description: prints using  printf*
+*
 * Return: is Always 0.
 */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
