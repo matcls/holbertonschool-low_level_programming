f7124fb93b68e4983893b31d868c655145aa4447
matcls <manueltorresvesga@gmail.com>
2020-02-06 18:41:57 -0500

    9-print_comb
    Signed-off-by: matcls <manueltorresvesga@gmail.com>

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: prints all possible combinations of single-digit numbers.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
