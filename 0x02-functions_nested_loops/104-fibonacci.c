a2cf40205cf3c75ac0670ab665d72da1815669db
matcls <manueltorresvesga@gmail.com>
2020-02-12 20:36:16 -0500

    added 104-fibonacci.c, prints the first 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,22 +10,44 @@
 
 int main(void)
 {
-	int i;
-	/*Is a better way to declare and assig variables?*/
-	float a, b, f;
+	int i, o1, o2; /*Is a better way to declare and assig variables?*/
+	long int a, b, f, p1, p2, o3;
 
 	a = 1;
 	b = 2;
-
-	printf("%.0f, %.0f", a, b);
-	for (i = 0; i < 95; i++)
+	o1 = o2 = 1;
+	printf("%ld, %ld", a, b);
+	for (i = 0; i < 96; i++) /* print fibonacci numbers  before overflow */
 	{
-		f = a + b;
-		a = b;
-		b = f;
-		printf(", %.0f", f);
+		if (o1)
+		{
+			f = a + b;
+			printf(", %ld", f);
+			a = b;
+			b = f;
+		}
+		else /* we go for overflow */
+		{
+			if (o2)
+			{
+				p1 = a % 1000000000;
+				p2 = b % 1000000000;
+				a = a / 1000000000;
+				b = b / 1000000000;
+				o2 = 0;
+			}
+			o3 = p1 + p2;
+			f = a + b + (o3 / 1000000000);
+			printf(", %ld", f);
+			printf("%ld", o3 % 1000000000);
+			a = b;
+			p1 = p2;
+			b = f;
+			p2 = (o3 % 1000000000);
+		}
+		if (((a + b) < 0) && o1 == 1)
+			o1 = 0;
 	}
 	printf("\n");
-
 	return (0);
 }
