ddea66d84e431826aece50072c7cc09c9cdd0aa1
matcls <manueltorresvesga@gmail.com>
2020-02-13 08:51:51 -0500

    added 0-isupper.c thath checks if input is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase characte
+ *
+ * @result: value returned if is or not uppercase
+ * @c: input to be evaluated
+ *
+ * Return: if uppercase 1 otherwise 0
+ */
+
+int _isupper(int c)
+{
+	int result;
+	/*Checks from A to Z*/
+	if (c >= 65 && c <= 90)
+	{
+		result = 1;
+		return (result);
+	}
+	else
+	return (0);
+}
+
