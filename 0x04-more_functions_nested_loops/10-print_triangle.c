54889e63a5b659b79674e95388e2a3743506e948
matcls <manueltorresvesga@gmail.com>
2020-02-13 15:42:15 -0500

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *print_triangle - prints a triangle, followed by a new line.
+ * print_triangle - prints a triangle, followed by a new line.
  *
- *
+ * @size: triangle size
  *
  * Return: 0
  */
@@ -12,7 +12,7 @@ void print_triangle(int size)
 {
 	int base, high;
 
-	if (size == 0)
+	if (size >= 0)
 	{
 		_putchar('\n');
 	}
