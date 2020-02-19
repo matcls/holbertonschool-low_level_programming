7318c8df39508f8bf228c400f90d646d9cc16ff2
matcls <manueltorresvesga@gmail.com>
2020-02-18 19:38:50 -0500

    added 100-atoi.c that converts a string into integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s:  string
+ *
+ * Return: an integer from s or 0 if is not a number
+ */
+
+int _atoi(char *s)
+{
+	int runner, minus, num, letter, retrieved;
+	runner = num = letter = retrieved = 0;
+	minus = 1;
+
+	while (*s)
+	{
+		if (s[runner] == 45)
+			minus *= -1;
+		if (s[runner] >= 48 && s[runner] <= 57)
+		{
+			retrieved = retrieved * 10 + (s[runner] - 48);
+			num = 1;
+		}
+		if (!(s[runner] >= 48 && s[runner] <= 57))
+		{
+			if (num == 1)
+				letter = 1;
+			if (letter == 1)
+			break;
+		}
+		runner++;
+	}
+ 	return (retrieved * minus);
+}
