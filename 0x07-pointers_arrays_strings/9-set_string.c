5e292924097c2e3ba79aa06af76a34925cc0d231
matcls <manueltorresvesga@gmail.com>
2020-02-24 12:55:38 -0500

    fixed 9-set_string.c

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * set_string -
+ * set_string - sets the value of a pointer to a char
  *
- * Return: 
+ * @s:  memory address from pointer to change
+ * @to: pointer to match
+ *
+ * Return: nothing
  */
+
 void set_string(char **s, char *to)
 {
-
+	*s = to;
 }
