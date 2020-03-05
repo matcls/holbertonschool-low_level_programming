1767d6edb8cfd9795d540175b4c89f3396cfe555
matcls <manueltorresvesga@gmail.com>
2020-03-05 18:23:40 -0500

    added advanced tasks
    Signed-off-by: matcls <manueltorresvesga@gmail.com>

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,11 +1,29 @@
 #include "holberton.h"
 
 /**
- * _realloc -
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: is the size, in bytes, of the allocated space for ptr
+ * @new_size: is the new size, in bytes of the new memory block
  *
- * Return: 
+ * Return: a pointer to the reallocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	if (new_size == old_size)
+		return (ptr);
 
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size * sizeof(char));
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "holberton.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: arguments matrix
+ *
+ * If the number of arguments is incorrect, print Error, followed by a new line
+ * and exit with a status of 98 . num1 and num2 should only be composed
+ * of digits. If not, print Error, followed by a new line, and exit with a
+ * status of 98
+ *
+ * Return: 0
+ */
+
 
 int main(int argc, char *argv[])
 {
 	long int mul;
 	long int num1;
 	long int num2;
-	long int plus;
+	long int i;
 
 	if (argc != 3)
 	{
@@ -16,15 +27,15 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	for (plus = 0; argv[1][plus]; plus++)
-		if (!isdigit(argv[1][plus]))
+	for (i = 0; argv[1][i]; i++)
+		if (!isdigit(argv[1][i]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
 
-	for (plus = 0; argv[2][plus]; plus++)
-		if (!isdigit(argv[2][plus]))
+	for (i = 0; argv[2][i]; i++)
+		if (!isdigit(argv[2][i]))
 		{
 			printf("Error\n");
 			exit(98);
@@ -32,6 +43,8 @@ int main(int argc, char *argv[])
 
 	num1 = atol(argv[1]);
 	num2 = atol(argv[2]);
+
+	/*working in a solution for big numbers*/
 	mul = num1 * num2;
 
 	printf("%ld\n", mul);
