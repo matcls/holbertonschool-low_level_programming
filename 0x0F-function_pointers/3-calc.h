2a9c5a469f688a7d60668381dcfd3cb6aa5544cd
matcls <manueltorresvesga@gmail.com>
2020-03-11 15:57:23 -0500

    fix for 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -13,6 +13,8 @@ typedef struct op
 		int (*f)(int a, int b);
 	} op_t;
 
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
