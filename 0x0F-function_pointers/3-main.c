982a5739ad3e075bc90d35eb9c602d726b036347
matcls <manueltorresvesga@gmail.com>
2020-03-11 16:34:51 -0500

    fix for 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,24 +2,24 @@
 #include "function_pointers.h"
 
 /**
- * main - performs simple operations and prints the result of the operation,
- * followed by a new line
+ * main - p1rints the result of the operation, followed by a new line
  * @argc: the number of args
  * @argv: array of arguments
  *
- * Return: 0.
+ * Return: Always 0.
  */
 int main(int argc, char **argv)
 {
 	int (*operator)(int, int);
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
+
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
 
 	operator = get_op_func(argv[2]);
 	if (!operator)
