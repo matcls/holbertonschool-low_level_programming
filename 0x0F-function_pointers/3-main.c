62e3c7b8b4a59f30fc670d80b4b17763dff77b4c
matcls <manueltorresvesga@gmail.com>
2020-03-11 15:47:03 -0500

    added main file for 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+/**
+ * main - performs simple operations and prints the result of the operation,
+ * followed by a new line
+ * @argc: the number of args
+ * @argv: array of arguments
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	int (*operator)(int, int);
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	operator = get_op_func(argv[2]);
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operator(a, b));
+	return (0);
+}
