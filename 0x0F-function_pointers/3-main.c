8b5cdeb792e6be72c212d2f2c94b7cf00c7243bd
matcls <manueltorresvesga@gmail.com>
2020-03-11 17:05:37 -0500

    fix for 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,16 +10,15 @@
  */
 int main(int argc, char **argv)
 {
-	int (*operator)(int, int), a ,b;
+	int (*operator)(int, int);
+	int a = atoi(argv[1]);
+	int b = atoi(argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 
 	operator = get_op_func(argv[2]);
 	if (!operator)
