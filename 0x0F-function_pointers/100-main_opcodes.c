e434fa4e36d8038a681f1ba103006c4047d1e8f4
matcls <manueltorresvesga@gmail.com>
2020-03-12 21:54:20 -0500

    add advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,24 @@
 #include "function_pointers.h"
 
 /**
- * op_sub -
+ * main - prints the opcodes of its own main function.
+ * @argc: number of arguments
+ * @argv: matrix of arguments
  *
- * Return: 
+ * Return: 0
  */
-int op_sub(int a, int b)
+int main(int argc, char **argv)
 {
+	char *p = (char *)main;
+	int b;
 
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
 }
+
