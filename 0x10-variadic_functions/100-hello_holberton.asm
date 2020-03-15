d9dad20b09cce2c40e7ebdd9ea075f994e16764c
matcls <manueltorresvesga@gmail.com>
2020-03-15 13:26:23 -0500

    fix for 4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,15 @@
-section .data
-    msg db "Hello Holberton!",10      ; 10 is the ASCII code for a new line (LF)
-
-section .text
-    global main
+global main
 
 main:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, 13
+    mov rdx, 17
     syscall
     
     mov rax, 60
-    mov rdi, 0
+    mov rdi, rdi
     syscall
+
+    section .data
+    msg db "Hello Holberton!",10      ; 10 is the ASCII code for a new line (LF)
