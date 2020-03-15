a4eab589a737131700d4624953b26095fef5754f
matcls <manueltorresvesga@gmail.com>
2020-03-15 13:23:45 -0500

    fix for 4

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,50 +1,16 @@
-	.file	"hello.c"
-	.intel_syntax noprefix
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton\n"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+    msg db "Hello Holberton!",10      ; 10 is the ASCII code for a new line (LF)
+
+section .text
+    global main
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	push	rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register 6
-	sub	rsp, 16
-	mov	DWORD PTR -4[rbp], edi
-	mov	QWORD PTR -16[rbp], rsi
-	mov	edx, 17
-	lea	rsi, .LC0[rip]
-	mov	edi, 1
-	call	write@PLT
-	mov	eax, 0
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.2.1-9ubuntu2) 9.2.1 20191008"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
