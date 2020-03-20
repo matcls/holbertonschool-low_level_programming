0ef252c77b2ae2b0699071fa1f43c8861b8601fa
matcls <manueltorresvesga@gmail.com>
2020-03-19 19:56:38 -0500

    fix for task 2.add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,27 +4,31 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @
+ * @head: pointer to the address of the first element
+ * @str: string to be added
+ *
  * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t tmp;
-	char *struc = strdup(str);
+	size_t node = 0;
+	list_t *newnode = NULL;
 
-	list_t *newnode = (list_t *)malloc(sizeof(list_t));
+	if (!head)
+		return (NULL);
 
+	newnode = malloc(sizeof(list_t));
 	if (!newnode)
 		return (NULL);
 
-	while (str[temp])
-		temp++;
+	while (str[node])
+		node++;
 
-	newnode->str = struc;
-	newnode->len = tmp;
+	newnode->str = strdup(str);
+	newnode->len = node;
 	newnode->next = *head;
 	*head = newnode;
 
 	return (*head);
-
 }
+
