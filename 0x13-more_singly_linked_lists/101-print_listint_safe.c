8bee781cf60e9f481995e0566303eca6b4c18139
matcls <manueltorresvesga@gmail.com>
2020-03-23 14:49:23 -0500

    added task 12. Print (safe version) 	* prints a listint_t linked list. 	* Prototype: size_t print_listint_safe(const listint_t *head); 	* Returns: the number of nodes in the list 	* This function can print lists with a loop 	* You should go through the list only once 	* If the function fails, exit the program with status 98 	* Output format: see example

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the list to be printed
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *ymp = malloc(sizeof(listint_t));
+	if (!ymp)
+		exit(98);
+
+	if (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head > head->next)
+			return (print_listint_safe(head->next) + 1);
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			return (1);
+		}
+	}
+	return (0);
+}
