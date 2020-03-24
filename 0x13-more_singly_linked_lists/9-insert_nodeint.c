073f6dab63dfa122ad8dc19949c76b2dc17e3041
matcls <manueltorresvesga@gmail.com>
2020-03-24 11:26:53 -0500

    betty style for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position. 
+ * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: pointer to the address of the head of the list.
  * @idx: the index of the list where the new node should be added.
  * @n: value to add to the new node.
