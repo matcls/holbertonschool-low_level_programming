905ac84effb75da5026a40984221cf1e08efe8b0
matcls <manueltorresvesga@gmail.com>
2020-03-26 16:40:06 -0500

    added task 2. 10  function that returns the value of a bit at a given index.

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit value to get
+ * @index: Te index, starting from 0 of the bit to get
+ *
+ * Return: The value of bit at index or -1 if an error occured.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((index < (sizeof(n) * 4)) ? (int)(n >> index) & 7 : -1);
+}
