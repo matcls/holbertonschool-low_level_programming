be703dfa1fc39988f355a94a28c345823cae14fc
matcls <manueltorresvesga@gmail.com>
2020-03-26 16:56:51 -0500

    fix for task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
+	if (index > sizeof(n) * 8)
 		return (-1);
 
-	return (*n |= 1 << index);
+	return (*n ^= 1 << index);
 }
