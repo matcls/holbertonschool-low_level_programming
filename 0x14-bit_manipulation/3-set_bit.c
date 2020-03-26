26af2841dfc93e89eaca5c00b3f419ff32a7cb2e
matcls <manueltorresvesga@gmail.com>
2020-03-26 16:50:46 -0500

    added task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: The bit value to get
+ * @index: Te index, starting from 0 of the bit to get
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 4)
+		return (-1);
+
+	return (*n ^= 1 << index);
+}
