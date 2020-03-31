b397e6a7589947fd24979f9d6371a71f3983fb1d
matcls <manueltorresvesga@gmail.com>
2020-03-31 18:48:02 -0500

    added task 3. cp: Program that copies the content of a file to another file. 	Usage: cp file_from file_to 	if the number of argument is not the correct one, exit with code 97 and print Usage: cp file_from file_to, followed by a new line, on the POSIX standard error 	if file_to already exists, truncate it 	if file_from does not exist, or if you can not read it, exit with code 98 and print Error: Can't read from file NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error 	where NAME_OF_THE_FILE is the first argument passed to your program 	if you can not create or if write to file_to fails, exit with code 99 and print Error: Can't write to NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error 	where NAME_OF_THE_FILE is the second argument passed to your program 	if you can not close a file descriptor , exit with code 100 and print Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error 	where FD_VALUE is the value of the file descriptor 	Permissions of the created file: rw-rw-r--. If the file already exists, do not change the permissions 	You must read 1,024 bytes at a time from the file_from to make less system calls. Use a buffer 	You are allowed to use dprintf

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: the number of arguments.
+ * @argv: arguments.
+ * Description:  description
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int src, dest, rd, wt;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, ERROR97), exit(97);
+
+	src = open(argv[1], O_RDONLY);
+	if (src == -1)
+		dprintf(STDERR_FILENO, ERROR98,	argv[1]), exit(98);
+
+	dest = open(argv[2], (O_CREAT | O_TRUNC | O_WRONLY), 0664);
+
+	while ((rd = read(src, buffer, 1024)) > 0)
+	{
+		wt = write(dest, buffer, rd);
+		if ((wt != rd) || (wt == -1))
+			dprintf(STDERR_FILENO, ERROR99, argv[2]), exit(99);
+	}
+
+	if (rd == -1)
+		dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
+
+	if (close(src))
+		dprintf(STDERR_FILENO, ERROR100, src), exit(100);
+
+	if (close(dest))
+		dprintf(STDERR_FILENO, ERROR100, dest), exit(100);
+
+	return (0);
+}
+
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,6 +9,11 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define ERROR97  "Usage: cp file_from file_to\n"
+#define ERROR98  "Error: Can't read from file %s\n"
+#define ERROR99  "Error: Can't write to %s\n"
+#define ERROR100 "Error: Can't close fd %d\n"
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
