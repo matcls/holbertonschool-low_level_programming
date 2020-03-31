bb8afac5762163713ae2aa40e9f9b16de456c75b
matcls <manueltorresvesga@gmail.com>
2020-03-31 17:43:46 -0500

    added task 2. Speak gently, she can hear: Function that appends text at the end of a file.
    	Prototype: int append_text_to_file(const char *filename, char *text_content);
    	where filename is the name of the file and text_content is the NULL terminated string to add at the end of the file
    	Return: 1 on success and -1 on failure
    	Do not create the file if it does not exist
    	If filename is NULL return -1
    	If text_content is NULL, do not add anything to the file. Return 1 if the file exists and -1 if the file does not exist or if you do not have the required permissions to write the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	write(file, text_content, strlen(text_content));
+	close(file);
 
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file -
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: a pointer to the file.
+ * @text_content: the string to add at the end of the file.
  *
- * Return: 
+ * Return: 1 on success and -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file;
 
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content)
+		write(file, text_content, strlen(text_content));
+
+	close(file);
+
+	return (1);
 }
