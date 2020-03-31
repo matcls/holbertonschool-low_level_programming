af4cc53e5d34bb36196348b92d8e51239ad2ad6c
matcls <manueltorresvesga@gmail.com>
2020-03-31 16:19:08 -0500

    added task 1. Under the snow: Function that creates a file. 	Prototype: int create_file(const char *filename, char *text_content); 	where filename is the name of the file to create and text_content is a NULL terminated string to write to the file 	Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…) 	The created file must have those permissions: rw-------. If the file already exists, do not change the permissions. 	if the file already exists, truncate it 	if filename is NULL return -1 	if text_content is NULL create an empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ * create_file - creates a file.
+ * @filename: a pointer to the file to write.
+ * @text_content: string to write to the file.
  *
- * Return: 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
+	int file;
 
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	file  = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, strlen(text_content));
+
+	return (1);
 }
