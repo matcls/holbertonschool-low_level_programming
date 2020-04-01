a30f570fa8210d298d6709b8770d36867b71561f
matcls <manueltorresvesga@gmail.com>
2020-03-31 19:16:52 -0500

    test file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ * @argc: the number of arguments.
+ * @argv: arguments.
+ * Description:  description
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
