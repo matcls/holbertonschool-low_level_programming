0a689760b76fdf03e0a17d46d33d3569de161198
matcls <manueltorresvesga@gmail.com>
2020-03-31 20:13:44 -0500

    test conditions for task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,43 +1,43 @@
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#define RWRWR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define ERR ("Error: Can't close fd %d\n")
+#define NOWRITE ("Error: Can't write to %s\n")
+#define NOREAD ("Error: Can't read from file %s\n")
+#define USAGE ("Usage: cp file_from file_to\n")
 /**
- * main - copies the content of a file to another file.
- * @argc: the number of arguments.
- * @argv: arguments.
- * Description:  description
+ * main - reads a text file and prints it to the POSIX stdout.
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: 0
+ * Return: Always 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int src, dest, rd, wt;
-	char buffer[1024];
+	int fd_from, fd_to, bufflen, w;
+	char buff[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, ERROR97), exit(97);
-
-	src = open(argv[1], O_RDONLY);
-	if (src == -1)
-		dprintf(STDERR_FILENO, ERROR98,	argv[1]), exit(98);
-
-	dest = open(argv[2], (O_CREAT | O_TRUNC | O_WRONLY), 0664);
-
-	while ((rd = read(src, buffer, 1024)) > 0)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+		dprintf(STDERR_FILENO, NOREAD, argv[1]), exit(98);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, RWRWR);
+	if (fd_to < 0)
+		dprintf(STDERR_FILENO, NOWRITE, argv[2]), exit(99);
+	while ((bufflen = read(fd_from, buff, 1024)) > 0)
 	{
-		wt = write(dest, buffer, rd);
-		if ((wt != rd) || (wt == -1))
-			dprintf(STDERR_FILENO, ERROR99, argv[2]), exit(99);
+		w = write(fd_to, buff, bufflen);
+		if (w  != bufflen || w < 0)
+			dprintf(STDERR_FILENO, NOWRITE, argv[2]), exit(99);
 	}
-
-	if (rd == -1)
-		dprintf(STDERR_FILENO, ERROR98, argv[1]), exit(98);
-
-	if (close(src))
-		dprintf(STDERR_FILENO, ERROR100, src), exit(100);
-
-	if (close(dest))
-		dprintf(STDERR_FILENO, ERROR100, dest), exit(100);
-
-	return (0);
+	if (bufflen < 0)
+		dprintf(STDERR_FILENO, NOREAD, argv[1]), exit(98);
+	if ((close(fd_from)) < 0)
+		dprintf(STDERR_FILENO, ERR, fd_from), exit(100);
+	if ((close(fd_to)) < 0)
+		dprintf(STDERR_FILENO, ERR, fd_to), exit(100);
+	return (EXIT_SUCCESS);
 }
-
