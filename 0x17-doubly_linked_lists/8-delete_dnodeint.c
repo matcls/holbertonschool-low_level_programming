fe6b9195a5896fa6100d9cdd8933d40d3d0dddb0
matcls <manueltorresvesga@gmail.com>
2020-05-03 21:08:11 -0500

    fix betty style for task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,16 +19,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		continue;
 	if (!tmp)
 		return (-1);
-	
+
 	if (tmp == *head)
 		*head = tmp->next;
-	
+
 	if (tmp->prev)
 		tmp->prev->next = tmp->next;
-	
+
 	if (tmp->next)
 		tmp->next->prev = tmp->prev;
-	
+
 	free(tmp);
 	return (1);
 }
